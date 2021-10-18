e2623bf977542820d8a60ed7e8743b8dedbb69ea
Sven Marnach <sven@mozilla.com>
2021-10-18 10:20:04 +0200

    Stop using a RefCell for the history.

--- src/history.rs
+++ src/history.rs
@@ -1,5 +1,4 @@
 use std::{
-    cell::RefCell,
     collections::{BTreeMap, HashSet},
     path::PathBuf,
 };
@@ -7,7 +6,7 @@ use std::{
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Default, Deserialize, Serialize)]
+#[derive(Debug, Clone, Default, Deserialize, Serialize)]
 #[serde(default)]
 struct HistoryConfig {
     account: Option<String>, // deprecated
@@ -17,7 +16,7 @@ struct HistoryConfig {
 
 #[derive(Debug)]
 pub struct History {
-    config: RefCell<HistoryConfig>,
+    config: HistoryConfig,
     max_last_accounts: usize,
     path: PathBuf,
 }
@@ -32,44 +31,32 @@ impl History {
             config.last_accounts.push(account);
         }
         Ok(Self {
-            config: RefCell::new(config),
+            config,
             max_last_accounts,
             path,
         })
     }
 
     pub fn account_index(&self, account: &str) -> Option<usize> {
-        self.config
-            .borrow()
-            .last_accounts
-            .iter()
-            .position(|x| x == account)
+        self.config.last_accounts.iter().position(|x| x == account)
     }
 
     pub fn default_account(&self) -> Option<String> {
-        self.config
-            .borrow()
-            .last_accounts
-            .first()
-            .map(ToOwned::to_owned)
+        self.config.last_accounts.first().map(ToOwned::to_owned)
     }
 
     pub fn default_role(&self, account: &str) -> Option<String> {
-        self.config
-            .borrow()
-            .roles
-            .get(account)
-            .map(ToOwned::to_owned)
+        self.config.roles.get(account).map(ToOwned::to_owned)
     }
 
     pub fn update(&self, account: &str, role: &str) -> Result<()> {
-        let mut config = self.config.borrow_mut();
+        let mut config = self.config.clone();
         config.last_accounts.insert(0, account.to_owned());
         let mut seen = HashSet::new();
         config.last_accounts.retain(|x| seen.insert(x.to_owned()));
         config.last_accounts.truncate(self.max_last_accounts);
         config.roles.insert(account.to_owned(), role.to_owned());
-        let config_toml = &toml::to_vec(&*config)?;
+        let config_toml = &toml::to_vec(&config)?;
         Ok(std::fs::write(&self.path, config_toml)?)
     }
 }
